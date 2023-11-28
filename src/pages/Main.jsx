f9e425cecd888fd67aceb4b2a73c7eca051a4489
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-11-28 11:52:18 +0100

    init

---
+++ src/pages/Main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Pages.scss";
+
+const Main = () => {
+  return <div className="mainpage_container"></div>;
+};
+
+export default Main;
---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+import Main from "./Main";
+
+// export { HomePage, AboutPage, OptionsPage, ContactPage, LocatePage };
+
+export default Main;
